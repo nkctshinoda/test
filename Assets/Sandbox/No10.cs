0a558f2358c76a286c44f74bb7b8da35f4a8fd35
nkckitamura <ybeicun65@gmail.com>
2024-12-10 11:47:55 +0900

    call

---
+++ Assets/Sandbox/No10.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns10
+{
+    public class No10 : MonoBehaviour
+    {
+        public int Call()
+        {
+            return 10;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
