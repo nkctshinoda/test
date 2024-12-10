1753cd9d1b90a31fd9975cceb60085b9e974ca2f
NkcIsazi0918 <nkcisyupractical1@gmail.com>
2024-12-10 12:28:53 +0900

    Callメソッドの再追加

---
+++ Assets/Sandbox/No02.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns02
+{
+    /// <summary>
+    /// 出席番号を返すメソッド
+    /// </summary>
+    public class No02 : MonoBehaviour
+    {
+        public int Call()
+        {
+            return 02;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+    }
+}
