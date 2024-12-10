c441ef5ca527b05cac746995109bf0f95ebb63a2
nkcgs1tanaka25 <nkcgs1.25tanaka@gmail.com>
2024-12-10 12:30:18 +0900

    Call

--- Assets/Sandbox/No25.cs
+++ Assets/Sandbox/No25.cs
@@ -6,6 +6,10 @@ namespace Ns25
 {
     public class No25 : MonoBehaviour
     {
+        public int Call()
+        {
+            return 25;
+        }
         // Start is called before the first frame update
         void Start()
         {
