b0364fb586a5a2fbbd881885c4f74d98d06f2ae3
Inada0308 <ia2248742@gmail.com>
2024-12-10 11:43:25 +0900

    No07クラスを作成

---
+++ Assets/Sandbox/No07.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Ns07
+{
+    public class No07 : MonoBehaviour
+    {
+        public int Call()
+        {
+            return 0;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("07");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
