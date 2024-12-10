d6478e0bfb8737a4b513151d158ff0491016670f
nkctakara <yitengbaoliang@gmail.com>
2024-12-10 12:19:35 +0900

    No5クラスの作成

---
+++ Assets/Sandbox/No05.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns05
+{
+    public class No05 : MonoBehaviour
+    {
+        public int Call()
+        {
+            return 5;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
