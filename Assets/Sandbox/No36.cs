abe84fa3d324f78307b03d12c769d62d6ef63d01
Misaki0315 <gakugyou315@gmail.com>
2024-12-10 12:33:22 +0900

    Create No36.cs

---
+++ Assets/Sandbox/No36.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns36
+{
+    public class No36 : MonoBehaviour
+    {
+        public int Call()
+        {
+            return 36;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
