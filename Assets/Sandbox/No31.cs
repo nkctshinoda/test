3c842fa8ecd5c6bf605b17d4dc87197d78083c62
hara648 <sakuya5143@icloud.com>
2024-12-10 12:31:56 +0900

    Callメソッド

---
+++ Assets/Sandbox/No31.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns31
+{
+
+    public class No31 : MonoBehaviour
+    {
+        /// <summary>
+        /// 出席番号を返すクラス
+        /// </summary>
+        /// <returns>出席番号</returns>
+        public int Call()
+        {
+            return 31;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
