3abfebeb74d74fa498a60025e1cb6d403b0b0925
syouya96 <lingmuxiangye68@gmail.com>
2024-12-10 11:42:35 +0900

    Callメソッドの追加

---
+++ Assets/Sandbox/No20.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ns20
+{
+    public class No20 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("20");
+        }
+
+        public int call()
+        {
+            return 20;
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
