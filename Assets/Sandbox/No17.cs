857dd47c987ed009e834fb61b5e8a8c0e7669c60
Sh1roishi <shiroishi.yuto.1101@gmail.com>
2024-12-10 12:30:20 +0900

    No17クラス再追加

---
+++ Assets/Sandbox/No17.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Ns17
+{
+    public class No17 : MonoBehaviour
+    {
+        /// <summary>
+        /// 出席番号を返すメソッド
+        /// </summary>
+        /// <returns>出席番号</returns>
+        public int Call()
+        {
+            return 17;
+        }
+        void Start()
+        {
+            Debug.Log("No17");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
