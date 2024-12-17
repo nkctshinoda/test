af5cc37b1fa5722d5b8e9add11757daaa985a1cc
LanLaLi <kieulan9722@gmail.com>
2024-12-17 12:42:11 +0900

    uplate No27

--- Assets/No27/No27.cs
+++ Assets/No27/No27.cs
@@ -1,18 +1,49 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class No27 : MonoBehaviour
 {
+    public float speed = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+       
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+
     }
 }
