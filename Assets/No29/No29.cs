f4e24a8d058481b88e73faac3fc118e89f5845db
NakayamaSeishiro <nkc20242225@gmail.com>
2024-12-17 12:40:18 +0900

    Scripts Changes

--- Assets/No29/No29.cs
+++ Assets/No29/No29.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class No29 : MonoBehaviour
 {
 
+    float StateTime = 0f;
+
+
     void Start()
     {
         
@@ -12,6 +15,17 @@ public class No29 : MonoBehaviour
 
     void Update()
     {
-        
+        StateTime += Time.deltaTime;
+
+        if (StateTime >= 1.0f)
+        {
+            float Ran_pos_y = Random.Range(-5.0f, 5.0f);
+            float Ran_pos_x = Random.Range(-9.0f, 9.0f);
+
+            Vector2 nextPos = new Vector2(Ran_pos_x, Ran_pos_y);
+
+            this.transform.position = nextPos;
+            StateTime = 0f;
+        }
     }
 }
