5b544bc19de0f738941af6091768da5ca65ef1ac
nira952 <niragrass952@gmail.com>
2024-12-17 13:25:06 +0900

    Update No37

--- Assets/No37/No37.cs
+++ Assets/No37/No37.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class No37 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject circle;
+
+    float x;
+    float y;
+
+    Vector2 move;
+
     void Start()
     {
-        
+        x = 1;
+        y = 1;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (!circle.GetComponent<SpriteRenderer>().isVisible)
+        {
+            if(circle.transform.position.x < 0)
+            {
+                x = Random.Range(0,2);
+            }
+            else
+            {
+                x = Random.Range(-1, 0); 
+            }
+
+            if(circle.transform.position.y < 0)
+            {
+                y = Random.Range(0, 2);
+            }
+            else
+            {
+                y = Random.Range(-1, 0);
+            }
+
+
+        }
+
+
+        move = new Vector2(x, y) / 10;
+
+        circle.transform.Translate(move);
     }
 }
