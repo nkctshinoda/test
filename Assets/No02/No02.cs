d143056a983681357729bc403bb73a0eee1449fc
NkcIsazi0918 <nkcisyupractical1@gmail.com>
2024-12-17 12:42:58 +0900

    オブジェクトの動作の追加

--- Assets/No02/No02.cs
+++ Assets/No02/No02.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class No02 : MonoBehaviour
 {
+    float Hor = 2;
+    float Ver = -1;
+    float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class No02 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3 (Hor,Ver,0) * speed * Time.deltaTime);
     }
 }
