bb10d2fbc275eafbbadb4bb45064082c32bfc879
Inada0308 <ia2248742@gmail.com>
2024-12-17 12:35:54 +0900

    Update No07

--- Assets/No07/No07.cs
+++ Assets/No07/No07.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class No07 : MonoBehaviour
 {
+    public float speed = 2f;               
+    public float radius = 3f;              
+    public bool moveInCircle = true;       
+
+    private Vector3 startPosition;         
+    private float angle = 0f;              
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveInCircle)
+        {
+            
+            angle += speed * Time.deltaTime;  
+            float x = Mathf.Cos(angle) * radius;
+            float y = Mathf.Sin(angle) * radius;
+
+            transform.position = startPosition + new Vector3(x, y, 0f);
+        }
+        else
+        {
+            
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+         
+            if (transform.position.x > startPosition.x + 10f) 
+            {
+                transform.position = startPosition;
+            }
+        }
     }
 }
