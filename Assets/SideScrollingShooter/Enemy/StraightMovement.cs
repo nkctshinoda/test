cdc78da5efb465fb94be3c623d227ce5114726fd
Makoto Shinoda <makoto.shinoda@denpa.jp>
2025-01-10 18:13:29 +0900

    直進する敵クラス

---
+++ Assets/SideScrollingShooter/Enemy/StraightMovement.cs
@@ -0,0 +1,22 @@
+using System.Numerics;
+
+namespace SSShooter
+{
+    public class StraightMovement : EnemyMovementBase
+    {
+        public override void Move()
+        {
+
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
