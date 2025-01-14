0273bf15eed1e3c8450f42c285ad715928f542b8
Makoto Shinoda <makoto.shinoda@denpa.jp>
2025-01-14 11:12:35 +0900

    敵移動の派生クラス

---
+++ Assets/SideScrollingShooter/Enemy/E00.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SSShooter
+{
+    public class E00:EnemyMovementBase
+    {
+        public override void Move()
+        {
+            
+        }
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
