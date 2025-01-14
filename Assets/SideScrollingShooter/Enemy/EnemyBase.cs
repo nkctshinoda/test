7521b2345bcd81d936204704f2c553a8c4cfe73d
Makoto Shinoda <makoto.shinoda@denpa.jp>
2025-01-14 10:16:34 +0900

    敵の基底クラス

---
+++ Assets/SideScrollingShooter/Enemy/EnemyBase.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+/*
+ * クラス名の衝突などを防ぐためnamespaceを使う
+ * 派生クラスでoverrideするメソッドはvirtualキーワードをつけて仮想メソッドにしておく
+ * privateにすべきメソッドを派生クラスからでもアクセスできるようにprotectedにしておく
+ */
+
+namespace SSShooter
+{
+    /// <summary>
+    /// 敵キャラクターの基底クラス
+    /// </summary>
+    public class EnemyBase : MonoBehaviour
+    {
+        // --- 属性 ---
+        public int hp;                // 体力
+        public int score;             // 倒した時のスコア
+
+        // --- 操作 ---
+        // 初期化処理
+        public virtual void Initialize()
+        {
+        }
+
+        // 被弾処理
+        public virtual void TakeDamage(int damage)
+        {
+            hp -= damage;
+            if (hp <= 0)
+            {
+                Die();
+            }
+        }
+
+        // 死亡処理
+        protected virtual void Die()
+        {
+            // スコア加算処理などをここに記述
+            Destroy(gameObject);
+        }
+    }
+}
