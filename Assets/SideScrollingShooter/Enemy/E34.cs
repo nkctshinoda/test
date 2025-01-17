7cfbc4a99e58ecff497ad27a6c92c4653fdd2718
zyugyoRen <zyugyo2024@gmail.com>
2025-01-17 12:32:29 +0900

    E34の変更

--- Assets/SideScrollingShooter/Enemy/E34.cs
+++ Assets/SideScrollingShooter/Enemy/E34.cs
@@ -6,29 +6,30 @@ namespace SSShooter
     public class E34 : EnemyMovementBase
     {
         float c;
+        public float MoveArea;
         public override void Move()
         {
+            c -= Time.deltaTime;
+            if (c < 0f)
+            {
+                c = Random.Range(0.3f, 0.5f);
+                if (transform.position.y > MoveArea) Initialize(3f, new Vector2(-1f, Random.Range(-3f, -1f)));
+                else if (transform.position.y < -MoveArea) Initialize(3f, new Vector2(-1f, Random.Range(3f, 1f)));
+                else Initialize(3f, new Vector2(-1f, Random.Range(-3f, 3f)));
+            }
             Vector3 moveD = direction;
-            transform.position += moveD * speed * Time.deltaTime;
+            transform.position += moveD.normalized * speed * Time.deltaTime;
         }
         // Start is called before the first frame update
         protected override void Start()
         {
             Initialize(1f, Vector2.left);
-            transform.position = new Vector2(15, 0);
+            transform.position = new Vector2(15f, 0f);
         }
 
         // Update is called once per frame
         protected override void Update()
         {
-            c -= Time.deltaTime;
-            if(c < 0)
-            {
-                c = Random.Range(0.3f, 0.5f);
-                if (transform.position.y < 4 && transform.position.y > 3) Initialize(3, new Vector2(-1, Random.Range(-2, -1)));
-                else if(transform.position.y > -4 && transform.position.y < -3)Initialize(3,new Vector2(-1,Random.Range(2, 1)));
-                else Initialize(3, new Vector2(-1, Random.Range(-2, 3)));
-            }
             base.Update();
         }
     }
