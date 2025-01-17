e8d6ee6841325c0c57651ce3b0bb2660b2d1cd38
nkckitamura <ybeicun65@gmail.com>
2025-01-17 12:40:47 +0900

    E10作成

---
+++ Assets/SideScrollingShooter/Enemy/E10.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SSShooter
+{
+    public class E10 : EnemyMovementBase
+    {
+        public override void Move()
+        {
+            Vector3 moveDir = direction;
+            transform.position += moveDir * speed * Time.deltaTime;
+        }
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            Initialize(1f, Vector2.left);
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
